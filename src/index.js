39636e9ff4e225c1b5273532947ab2677fd00a38
anagha <anaghayawale7@gmail.com>
2025-09-27 23:32:24 +0530

    feat: added utils

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 //require("dotenv").config({ path: "/.env" });
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({ path: "./.env" });
 
-connectDB()
+connectDB().then( () => {
+    app.on("error", (error) => {
+        console.error("ERROR: ",error)
+    })
+
+    app.listen(process.env.PORT || 8000, (req, res) => {
+        console.log(`Listening on port ${process.env.PORT}`)
+    })
+}).catch((err) => {
+    console.error("MONGODB CONNECTION FAILED:  ",err)
+})
