8323170d1d362edcdee51c26985a7748056149bb
anagha <anaghayawale7@gmail.com>
2025-09-28 19:34:36 +0530

    feat: all schema updated

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -3,13 +3,14 @@ import mongoose, { Schema, Document, Model } from "mongoose";
 export interface IProduct extends Document {
   productId?: string;
   productName: string;
-  description?: string;
+  description: string;
   stockQuantity: number;
   lowStockThreshold: number;
-  createdAt?: Date;
-  updatedAt?: Date;
+  createdBy: mongoose.Types.ObjectId;
+  updatedBy: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
 }
-
 
 const ProductSchema: Schema<IProduct> = new Schema(
   {
@@ -22,6 +23,7 @@ const ProductSchema: Schema<IProduct> = new Schema(
     },
     description: {
       type: String,
+      required: [true, "Product description is required"],
       trim: true,
       maxlength: [500, "Description cannot exceed 500 characters"],
     },
@@ -37,17 +39,26 @@ const ProductSchema: Schema<IProduct> = new Schema(
       min: [0, "Low stock threshold cannot be negative"],
       default: 10,
     },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Created by user is required"],
+    },
+    updatedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Updated by user is required"],
+    },
   },
   {
     timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" },
-    versionKey: false,
     toJSON: {
-       transform: function(doc, ret) {
+      transform: function (doc, ret: any) {
         ret.productId = ret._id?.toString();
         delete ret._id;
         return ret;
-      }
-    }
+      },
+    },
   }
 );
 
@@ -62,7 +73,6 @@ ProductSchema.pre<IProduct>("save", function (next) {
 
   next();
 });
-
 
 export const Product: Model<IProduct> = mongoose.model<IProduct>(
   "Product",
---
+++ src/models/user.model.ts
@@ -0,0 +1,82 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+import bcrypt from "bcryptjs";
+import { Roles } from "@src/constants";
+
+
+export interface IUser extends Document {
+  userId?: string;
+  username: string;
+  password: string;
+  role: Roles;
+  createdAt: Date;
+  updatedAt: Date;
+comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const UserSchema: Schema<IUser> = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      trim: true,
+      minlength: [3, "Username must be at least 3 characters"],
+      maxlength: [30, "Username cannot exceed 30 characters"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      validate: {
+        validator: function (value: string) {
+          return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+            value
+          );
+        },
+        message:
+          "Password must contain at least 1 uppercase, 1 lowercase, 1 number, 1 special character and be at least 8 characters long",
+      },
+    },
+    role: {
+      type: String,
+      required: [true, "Role is required"],
+      enum: {
+        values: Object.values(Roles),
+        message: "Role must be either 'admin' or 'user'",
+      },
+      default: Roles.USER,
+    },
+  },
+  {
+    timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" },
+    toJSON: {
+      transform: function (doc, ret: any) {
+        ret.userId = ret._id?.toString();
+        delete ret._id;
+        delete ret.password;
+        return ret;
+      },
+    },
+  }
+);
+
+UserSchema.pre<IUser>("save", function (next) {
+  if (this.isModified("username")) {
+    this.username = this.username.trim().toLowerCase();
+  }
+  next();
+});
+
+UserSchema.pre<IUser>("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+UserSchema.methods.comparePassword = async function (
+  candidatePassword: string
+): Promise<boolean> {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+export const User: Model<IUser> = mongoose.model<IUser>("User", UserSchema);
